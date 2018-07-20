b21380d48f2bd4e6d768c9f04f6c64d7c3bb5824
Hassan A <has_is_ere@hotmail.com>
2018-07-20 10:35:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,17 +15,16 @@ function init() {
   
     function triggerKeyDown(key) {
     const keyboardEvent = new KeyboardEvent("keydown", { key });
-    document.body.dispatchEvent(keyboardEvent);
+    document.body.addEventListener('keydown', function(event){
+      window.alert("body was clicked!")
+  })
+  
   }
   
     for (let i = 0, l = codes.length; i < l; i++) {
         triggerKeyDown(codes[i]);
       }
   
-  // your code here
-  
-  const body = document.body.addEventListener('keydown', function(event){
-    window.alert("body was clicked!")
-  })
+
   
 }
