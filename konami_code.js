44ede65796fe33127f96c07511efb6bc59a6f21d
Hassan A <has_is_ere@hotmail.com>
2018-07-20 10:43:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -16,7 +16,27 @@ function init() {
   
   document.onkeydown = checkKey;
   
-  
+  function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+          document.getElementById("demo").innerHTML = Date();
+        // up arrow
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+    }
+
+}
+
+
 document.addEventListener("onkeydown", function(){
     document.getElementById("demo").innerHTML = Date();
 });
