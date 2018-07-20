ddcf5538d84de2496ff394fe338d77cedabb0513
Hassan A <has_is_ere@hotmail.com>
2018-07-20 13:47:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,33 +11,51 @@ const codes = [
   "a"
 ];
 
-var keyDownArr = []
-var achieved = []
+// var keyDownArr = []
+// var achieved = []
 
+// function init() {
+//   document.body.addEventListener('keydown', function(event){
+//     console.log("keydown: "+ event.key)
+    
+//     if (keyDownArr.length === 10){
+//       keyDownArr.shift()
+//       keyDownArr.push(event.key)
+//       konamiCodeAchieved()
+//     }else {
+//     keyDownArr.push(event.key)
+//     }
+    
+//   })
+// }
+
+// function konamiCodeAchieved(){
+//   for (let i=0; i<codes.length; i++){
+//     if (codes[i] != keyDownArr[i]){
+//       keyDownArr=[]
+//     } else {
+//       if (codes.length === keyDownArr.length){
+//         alert("Fook")
+//       }
+//     }
+//   }
+  
+  
+  
+var keys = [];
+const body = document.body;
 function init() {
-  document.body.addEventListener('keydown', function(event){
-    console.log("keydown: "+ event.key)
-    
-    if (keyDownArr.length === 10){
-      keyDownArr.shift()
-      keyDownArr.push(event.key)
-      konamiCodeAchieved()
-    }else {
-    keyDownArr.push(event.key)
-    }
-    
-  })
+   // your code here
 }
-
-function konamiCodeAchieved(){
-  for (let i=0; i<codes.length; i++){
-    if (codes[i] != keyDownArr[i]){
-      keyDownArr=[]
-    } else {
-      if (codes.length === keyDownArr.length){
-        alert("Fook")
-      }
+  body.addEventListener('keydown', function(event) {
+  keys.push(event.key);
+  for(let i = 0; i< keys.length; i++){
+    if(keys[i] != codes[i]){
+      keys = [];
     }
   }
-
-}
+ 
+ if(keys.length == codes.length){
+   alert('Hurray!');
+ }
+})
