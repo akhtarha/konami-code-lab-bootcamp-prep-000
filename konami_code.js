1c8efa5a2299324fd43aef88a7bbe66b4b7c5cf9
Hassan A <has_is_ere@hotmail.com>
2018-07-20 13:48:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,34 +11,34 @@ const codes = [
   "a"
 ];
 
-// var keyDownArr = []
-// var achieved = []
+var keyDownArr = []
+var achieved = []
 
-// function init() {
-//   document.body.addEventListener('keydown', function(event){
-//     console.log("keydown: "+ event.key)
+function init() {
+  document.body.addEventListener('keydown', function(event){
+    console.log("keydown: "+ event.key)
     
-//     if (keyDownArr.length === 10){
-//       keyDownArr.shift()
-//       keyDownArr.push(event.key)
-//       konamiCodeAchieved()
-//     }else {
-//     keyDownArr.push(event.key)
-//     }
+    if (keyDownArr.length === 10){
+      keyDownArr.shift()
+      keyDownArr.push(event.key)
+      konamiCodeAchieved()
+    }else {
+    keyDownArr.push(event.key)
+    }
     
-//   })
-// }
+  })
+}
 
-// function konamiCodeAchieved(){
-//   for (let i=0; i<codes.length; i++){
-//     if (codes[i] != keyDownArr[i]){
-//       keyDownArr=[]
-//     } else {
-//       if (codes.length === keyDownArr.length){
-//         alert("Fook")
-//       }
-//     }
-//   }
+function konamiCodeAchieved(){
+  for (let i=0; i<codes.length; i++){
+    if (codes[i] != keyDownArr[i]){
+      keyDownArr=[]
+    } else {
+      if (codes.length === keyDownArr.length){
+        alert("Fook")
+      }
+    }
+  }
   
   
   
