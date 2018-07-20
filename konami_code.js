3f4128aef430ebeaf4a1103e752568f3a5b728af
Hassan A <has_is_ere@hotmail.com>
2018-07-20 14:12:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -22,21 +22,16 @@ function init() {
   console.log("keyDownArrlength: "+keyDownArr.length)
   console.log(codes.length===keyDownArr.length)
   
-  if (keyDownArr.length === 9){
-    keyDownArr.push(event.key)
-    
-    for (let i=0; i<codes.length; i++){
-      if (codes[i] != keyDownArr[i]){
-        console.log(codes[i]+ " : "+ keyDownArr[i])
-        keyDownArr=[]
-      } else {
-        if (codes.length === keyDownArr.length){
-          alert("Fook");
-        }
+  keyDownArr.push(event.key)
+  for (let i=0; i<codes.length; i++){
+    if (codes[i] != keyDownArr[i]){
+      console.log(codes[i]+ " : "+ keyDownArr[i])
+      keyDownArr=[]
+    } else {
+      if (codes.length === keyDownArr.length){
+        alert("Fook");
       }
     }
-  }else {
-    keyDownArr.push(event.key)
   }
   })
 
