c29184b9920d0a14cab6c1f21f7701840ac0e50f
Hassan A <has_is_ere@hotmail.com>
2018-07-20 10:27:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,9 @@ const codes = [
 
 function init() {
   // your code here
+  const body = document.querySelector('body')
+  body.addEventListener('keydown', function(event){
+    alert("body was clicked!")
+  })
+  
 }
