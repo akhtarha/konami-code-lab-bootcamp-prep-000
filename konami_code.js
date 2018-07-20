1314ebe0d5b7e19601f5d81d9a7f21b60d2def95
Hassan A <has_is_ere@hotmail.com>
2018-07-20 13:58:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,49 +11,49 @@ const codes = [
   "a"
 ];
 
-// var keyDownArr = []
+var keyDownArr = []
 
-// function init() {
-  
-// }
-  
-//   const boday = document.body
-//   body.addEventListener('keydown', function(event){
-//   console.log("keydown: "+ event.key)
-//   console.log("keyDownArr: "+keyDownArr)
-  
-//   if (keyDownArr.length === 10){
-//     keyDownArr.shift()
-//     keyDownArr.push(event.key)
-//     for (let i=0; i<codes.length; i++){
-//       if (codes[i] != keyDownArr[i]){
-//         keyDownArr=[]
-//       } else {
-//         if (codes.length === keyDownArr.length){
-//           alert("Fook")
-//         }
-//       }
-//     }
-//   }else {
-//     keyDownArr.push(event.key)
-//   }
-//   })
-
-
-var keys = [];
-const body = document.body;
 function init() {
-  // your code here
+  
 }
-  body.addEventListener('keydown', function(event) {
-  keys.push(event.key);
-  for(let i = 0; i< keys.length; i++){
-    if(keys[i] != codes[i]){
-      keys = [];
+  
+  const boday = document.body
+  body.addEventListener('keydown', function(event){
+  console.log("keydown: "+ event.key)
+  console.log("keyDownArr: "+keyDownArr)
+  
+  if (keyDownArr.length === 10){
+    keyDownArr.shift()
+    keyDownArr.push(event.key)
+    for (let i=0; i<codes.length; i++){
+      if (codes[i] != keyDownArr[i]){
+        keyDownArr=[]
+      } else {
+        if (codes.length === keyDownArr.length){
+          alert("Fook")
+        }
+      }
     }
+  }else {
+    keyDownArr.push(event.key)
   }
+  })
+
+
+// var keys = [];
+// const body = document.body;
+// function init() {
+//   // your code here
+// }
+//   body.addEventListener('keydown', function(event) {
+//   keys.push(event.key);
+//   for(let i = 0; i< keys.length; i++){
+//     if(keys[i] != codes[i]){
+//       keys = [];
+//     }
+//   }
  
-if(keys.length == codes.length){
-  alert('Hurray!');
-}
-})
+// if(keys.length == codes.length){
+//   alert('Hurray!');
+// }
+// })
