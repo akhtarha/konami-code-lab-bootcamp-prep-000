439a811aec58d6f55724d3147601e2a4ba3001d7
Hassan A <has_is_ere@hotmail.com>
2018-07-20 13:56:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,10 +12,10 @@ const codes = [
 ];
 
 var keyDownArr = []
-var achieved = []
 
 function init() {
-  }
+  
+}
   
   const boday = document.body
   body.addEventListener('keydown', function(event){
@@ -40,20 +40,20 @@ function init() {
   })
 
 
-// var keys = [];
-// const body = document.body;
-// function init() {
-//   // your code here
-// }
-//   body.addEventListener('keydown', function(event) {
-//   keys.push(event.key);
-//   for(let i = 0; i< keys.length; i++){
-//     if(keys[i] != codes[i]){
-//       keys = [];
-//     }
-//   }
+var keys = [];
+const body = document.body;
+function init() {
+  // your code here
+}
+  body.addEventListener('keydown', function(event) {
+  keys.push(event.key);
+  for(let i = 0; i< keys.length; i++){
+    if(keys[i] != codes[i]){
+      keys = [];
+    }
+  }
  
-// if(keys.length == codes.length){
-//   alert('Hurray!');
-// }
-// })
+if(keys.length == codes.length){
+  alert('Hurray!');
+}
+})
