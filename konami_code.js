ed8479ddb1f9f6cd38e25090361b350b768b3310
Hassan A <has_is_ere@hotmail.com>
2018-07-20 13:55:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -40,20 +40,20 @@ function init() {
   })
 
 
-var keys = [];
-const body = document.body;
-function init() {
-   // your code here
-}
-  body.addEventListener('keydown', function(event) {
-  keys.push(event.key);
-  for(let i = 0; i< keys.length; i++){
-    if(keys[i] != codes[i]){
-      keys = [];
-    }
-  }
+// var keys = [];
+// const body = document.body;
+// function init() {
+//   // your code here
+// }
+//   body.addEventListener('keydown', function(event) {
+//   keys.push(event.key);
+//   for(let i = 0; i< keys.length; i++){
+//     if(keys[i] != codes[i]){
+//       keys = [];
+//     }
+//   }
  
- if(keys.length == codes.length){
-   alert('Hurray!');
- }
-})
+// if(keys.length == codes.length){
+//   alert('Hurray!');
+// }
+// })
